35f35d8f090c8678f2d88ecab054233d993f6e5f
diegosfb <diegosfbf@gmail.com>
2015-11-19 00:03:52 -0300

    Adding initial project files

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 100)
+            {
+                Console.Out.WriteLine("HOLA COMO VA");
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
